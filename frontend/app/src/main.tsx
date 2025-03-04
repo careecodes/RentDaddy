d01646697855679d946b23869a99fc14b457928c
hector3211 <hectororopesa5@gmail.com>
2025-03-04 14:18:02 -0500

    added: login form component

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -10,6 +10,7 @@ import ReusableComponents from "./components/ReusableComponents.tsx";
 import ProtectedRoutes from "./providers/ProtectedRoutes.tsx";
 import LoginForm from "./components/LoginForm.tsx";
 import ErrorNotFound from "./pages/Error404.tsx";
+import LoginForm from "./components/LoginForm.tsx";
 
 const queryClient = new QueryClient();
 
