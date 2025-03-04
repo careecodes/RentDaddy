4cdc02e8c41cf874491e6b017b2b44e297a3f2cf
hector3211 <hectororopesa5@gmail.com>
2025-03-04 14:18:02 -0500

    added: login form component

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./styles/styles.scss"
-import App from "./App.tsx"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/styles.scss";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
-import ReusableComponents from "./components/ReusableComponents.tsx"
+import ReusableComponents from "./components/ReusableComponents.tsx";
 
 // Protected Routes to be used for all routes that require authentication
 import ProtectedRoutes from "./providers/ProtectedRoutes.tsx";
+import LoginForm from "./components/LoginForm.tsx";
 
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -18,7 +18,6 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         {/* Routes: Container for all Route definitions */}
         <Routes>
-
           {/* Example and Explanation of Routes */}
           {/* 
             Routes are used to define the paths and components that will be rendered when a user navigates to a specific URL.
@@ -40,7 +39,7 @@ createRoot(document.getElementById("root")!).render(
 
           {/* Authentication Route Group */}
           <Route path="authentication">
-            <Route path="login" element={<h1>Login</h1>} />
+            <Route path="login" element={<LoginForm />} />
 
             {/* Not sure we need this one since we are having the admins create a tenant */}
             {/* But maybe to register the init admin? */}
@@ -51,18 +50,33 @@ createRoot(document.getElementById("root")!).render(
           <Route element={<ProtectedRoutes />}>
             <Route path="admin">
               <Route index element={<h1>Admin Dashboard</h1>} />
-              <Route path="init-apartment-complex" element={<h1>Initial Admin Apartment Complex Setup</h1>} />
+              <Route
+                path="init-apartment-complex"
+                element={<h1>Initial Admin Apartment Complex Setup</h1>}
+              />
               <Route path="add-tenant" element={<h1>Add Tenant</h1>} />
-              <Route path="admin-view-and-edit-leases" element={<h1>Admin View & Edit Leases</h1>} />
-              <Route path="admin-view-and-edit-work-orders" element={<h1>Admin View & Edit Work Orders</h1>} />
+              <Route
+                path="admin-view-and-edit-leases"
+                element={<h1>Admin View & Edit Leases</h1>}
+              />
+              <Route
+                path="admin-view-and-edit-work-orders"
+                element={<h1>Admin View & Edit Work Orders</h1>}
+              />
             </Route>
 
             {/* Tenant Route Group */}
             <Route path="tenant">
               <Route index element={<h1>Tenant Dashboard</h1>} />
               <Route path="guest-parking" element={<h1>Guest Parking</h1>} />
-              <Route path="digital-documents" element={<h1>Digital Documents</h1>} />
-              <Route path="work-orders-and-complaints" element={<h1>Work Orders & Complaints</h1>} />
+              <Route
+                path="digital-documents"
+                element={<h1>Digital Documents</h1>}
+              />
+              <Route
+                path="work-orders-and-complaints"
+                element={<h1>Work Orders & Complaints</h1>}
+              />
             </Route>
           </Route>
 
@@ -71,5 +85,5 @@ createRoot(document.getElementById("root")!).render(
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>
-)
+  </StrictMode>,
+);
