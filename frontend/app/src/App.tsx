a56c7a44a33e8b5d27617f1999753c913d5f06e8
DailyDisco <actual.mensch0r@icloud.com>
2025-03-05 19:16:15 -0600

    Style the Clerk Auth Demo

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -35,39 +35,44 @@ function App() {
           {/* Title */}
           <h4>Clerk Auth Demo</h4>
           <SignedIn>
-            <div className="d-flex gap-3 align-items-center mb-3">
-              <UserButton />
+            <div className="d-flex flex-column justify-content-center align-items-center my-5">
+              <p className="fs-2 text-center text-danger">NOTICE!</p>
+              <p className="fs-2 text-center">We are routed back to this page after authentication, but we can also route to the dashboard pages based off the user's role.
+              </p>
             </div>
-            <div className="d-flex gap-3 align-items-center mb-3">
+            <div className="d-flex gap-4 align-items-center mb-4">
+              <UserButton size="lg" />
+            </div>
+            <div className="d-flex gap-4 align-items-center mb-4">
               <Link to="test-go-backend">
-                <Button type="primary" className="my-2">Test Go Backend</Button>
+                <Button type="primary" size="large" className="my-3">Test Go Backend</Button>
               </Link>
               <SignOutButton>
-                <Button danger className="my-2">
+                <Button danger size="large" className="my-3">
                   Sign Out
                 </Button>
               </SignOutButton>
             </div>
-            <div className="d-flex flex-column gap-2 text-center">
-              <p className="text-muted mb-1">Name: {user?.fullName}</p>
-              <p className="text-muted mb-1">Email: {user?.emailAddresses[0].emailAddress}</p>
-              <p className="text-muted">Role: {user?.publicMetadata.role}</p>
+            <div className="d-flex flex-column gap-3 text-center">
+              <p className="text-muted mb-2 fs-3">Name: {user?.fullName}</p>
+              <p className="text-muted mb-2 fs-3">Email: {user?.emailAddresses[0].emailAddress}</p>
+              <p className="text-muted fs-3">Role: {user?.publicMetadata.role}</p>
             </div>
           </SignedIn>
           <SignedOut>
-            <div className="d-flex gap-3 justify-content-center">
+            <div className="d-flex gap-4 justify-content-center">
               <Link to="/auth/login">
-                <Button type="primary" className="my-2">
+                <Button type="primary" size="large" className="my-3">
                   Our Sign In
                 </Button>
               </Link>
               <SignInButton>
-                <Button className="my-2">
+                <Button size="large" className="my-3">
                   Clerk Sign In
                 </Button>
               </SignInButton>
               <SignInButton mode="modal">
-                <Button className="my-2">
+                <Button size="large" className="my-3">
                   Clerk Sign In (Modal)
                 </Button>
               </SignInButton>
