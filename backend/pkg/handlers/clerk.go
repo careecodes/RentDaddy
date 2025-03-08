ca27f894aa961f038eea099227fb86edaf37222c
hector3211e <hectororopesa5@gmail.com>
2025-03-08 17:49:54 -0500

    added: retries

--- backend/pkg/handlers/clerk.go
+++ backend/pkg/handlers/clerk.go
@@ -6,20 +6,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/clerk/clerk-sdk-go/v2/user"
 	"github.com/joho/godotenv"
 	svix "github.com/svix/svix-webhooks/go"
 )
 
-type UserMetaData map[string]interface{}
+type ClerkUserPublicMetaData map[string]interface{}
 
 type ClerkUserData struct {
-	ID             string       `json:"id"`
-	Email          string       `json:"email_address"`
-	FirstName      string       `json:"first_name"`
-	LastName       string       `json:"last_name"`
-	ProfileImage   string       `json:"profile_image_url"`
-	PublicMetaData UserMetaData `json:"public_metadata"`
+	ID             string          `json:"id"`
+	Email          string          `json:"email_address"`
+	FirstName      string          `json:"first_name"`
+	LastName       string          `json:"last_name"`
+	ProfileImage   string          `json:"profile_image_url"`
+	PublicMetaData json.RawMessage `json:"public_metadata"`
 }
 
 type ClerkWebhookPayload struct {
@@ -35,18 +37,18 @@ func Verify(payload []byte, headers http.Header) bool {
 
 	webhookSecret := os.Getenv("CLERK_WEBHOOK")
 	if webhookSecret == "" {
-		log.Println("[CLERK WEBHOOK] Environment variable is required")
+		log.Println("[CLERK_WEBHOOK] Environment variable is required")
 		return false
 	}
 	wh, err := svix.NewWebhook(webhookSecret)
 	if err != nil {
-		log.Printf("[CLERK WEBHOOK] Svix failed initailizing %v", err)
+		log.Printf("[CLERK_WEBHOOK] Svix failed initailizing %v", err)
 		return false
 	}
 
 	err = wh.Verify(payload, headers)
 	if err != nil {
-		log.Printf("[CLERK WEBHOOK]Invalid webhook signature: %v", err)
+		log.Printf("[CLERK_WEBHOOK]Invalid webhook signature: %v", err)
 		return false
 	}
 
@@ -56,28 +58,28 @@ func Verify(payload []byte, headers http.Header) bool {
 func ClerkWebhookHanlder(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Println("[CLERK WEBHOOK] Failed reading body")
+		log.Println("[CLERK_WEBHOOK] Failed reading body")
 		http.Error(w, "Failed reading body", http.StatusBadRequest)
 		return
 	}
 	defer r.Body.Close()
 
 	if !Verify(body, r.Header) {
-		log.Println("[CLERK WEBHOOK] Invalid webhook signature")
+		log.Println("[CLERK_WEBHOOK] Invalid webhook signature")
 		http.Error(w, "Invalid signature", http.StatusUnauthorized)
 		return
 	}
 
 	var payload ClerkWebhookPayload
 	if err := json.Unmarshal(body, &payload); err != nil {
-		log.Println("[CLERK WEBHOOK] Failed parsing payload")
+		log.Println("[CLERK_WEBHOOK] Failed parsing payload")
 		http.Error(w, "Invalid payload", http.StatusBadRequest)
 		return
 	}
 
 	var clerkUserData ClerkUserData
 	if err := json.Unmarshal(payload.Data, &clerkUserData); err != nil {
-		log.Println("[CLERK WEBHOOK] Failed parsing user data")
+		log.Println("[CLERK_WEBHOOK] Failed parsing user data")
 		http.Error(w, "Invalid user data", http.StatusBadRequest)
 		return
 	}
@@ -85,7 +87,7 @@ func ClerkWebhookHanlder(w http.ResponseWriter, r *http.Request) {
 	// Subscribed events
 	switch payload.Type {
 	case "user.created":
-		createUser(w, clerkUserData)
+		createUser(w, r, clerkUserData)
 	case "user.updated":
 		updateUser(w, clerkUserData)
 	case "user.deleted":
@@ -98,20 +100,55 @@ func ClerkWebhookHanlder(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"status":"received"}`))
 }
 
-func createUser(w http.ResponseWriter, userData ClerkUserData) {
-	// connect to db
-	log.Printf("[CLERK WEBHOOK] New user created: %s (%s)", userData.ID, userData.Email)
+func createUser(w http.ResponseWriter, r *http.Request, userData ClerkUserData) {
+	// Insert new user in DB
+
+	// Update clerk user metadata with DB ID, role, ect.
+	dummyMetadata := &ClerkUserPublicMetaData{
+		"dbID": 1,
+		"role": "tenant",
+	}
+
+	metadataBytes, err := json.Marshal(dummyMetadata)
+	if err != nil {
+		log.Printf("[CLERK_WEBHOOK] Error updating user with db credintials: %v", err)
+		metadataBytes = []byte("{}")
+	}
+	metadata := json.RawMessage(metadataBytes)
+
+	maxRetries := 3
+	for i := 0; i < maxRetries; i++ {
+		_, err = user.Update(r.Context(), userData.ID, &user.UpdateParams{
+			PublicMetadata: &metadata,
+		})
+		if err == nil {
+			break
+		}
+
+		log.Printf("[CLERK_WEBHOOK] Retry %d/%d Failed updating user %s metadata Error: %v", i+1, maxRetries, userData.ID, err)
+		time.Sleep(time.Duration(i+1) * time.Second)
+	}
+	if err != nil {
+		log.Printf("[CLERK_WEBHOOK] Error could not update user metadata: %v", err)
+		// Currently not erroring out
+
+		//    w.WriteHeader(http.StatusInternalServerError)
+		// return
+	}
+
+	log.Printf("[CLERK_WEBHOOK] New user created: %s (%s)", userData.ID, userData.Email)
 	w.WriteHeader(http.StatusCreated)
 }
 
 func updateUser(w http.ResponseWriter, userData ClerkUserData) {
-	// connect to db
-	log.Printf("[CLERK WEBHOOK] User updated: %s (%s)", userData.ID, userData.Email)
+	// Query user from DB
+	// Compare email, ect.
+	log.Printf("[CLERK_WEBHOOK] User updated: %s (%s)", userData.ID, userData.Email)
 	w.WriteHeader(http.StatusOK)
 }
 
 func deleteUser(w http.ResponseWriter, userData ClerkUserData) {
-	// connect to db
-	log.Printf("[CLERK WEBHOOK] User deleted: %s", userData.ID)
+	// Delete user from DB
+	log.Printf("[CLERK_WEBHOOK] User deleted: %s", userData.ID)
 	w.WriteHeader(http.StatusOK)
 }
